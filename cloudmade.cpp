e7dcd6713f3e7253b882a724a843b4b502f4071d
rzlmrn <117740852+rzlmrn@users.noreply.github.com>
2024-02-18 06:31:44 +0700

    add third line cloudmade.cpp
    add third line in ver-mail

--- cloudmade.cpp
+++ cloudmade.cpp
@@ -4,4 +4,5 @@ using namespace std;
 int main(){
   cout << "this program was made in cloud (main)" << endl;
   cout << "i add a newlines in cloud (main)" << endl;
+  cout << "i add a third line in cloud (vers-mail)" << endl;
 return 0;}
