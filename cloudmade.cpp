39c591185d5bbe483d6af01c1fe042f23e2d855b
Muhammad Rizal Nurhandaru <154496557+muhrizz@users.noreply.github.com>
2024-02-18 13:13:48 +0700

    Update fourth line cloudmade.cpp
    update by muhrizz in main

--- cloudmade.cpp
+++ cloudmade.cpp
@@ -5,4 +5,5 @@ int main(){
   cout << "this program was made in cloud (main)" << endl;
   cout << "i add a newlines in cloud (main)" << endl;
   cout << "i add a third line in cloud (vers-mail)" << endl;
+  cout << "i add a fourth line in cloud (muhrizz main)" << endl;
 return 0;}
